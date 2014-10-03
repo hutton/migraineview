2430b59250de83706197b76a1cb343e2749b434a
Simon <simon.hutton@gmail.com>
2014-10-03 12:05:00 +0100

    Move things around

--- migraine_statistics.py
+++ migraine_statistics.py
@@ -1,24 +1,9 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+__author__ = 'simonhutton'
+
 import calendar
 from collections import Counter
 import os
 import datetime
-from google.appengine.ext.webapp import template
 import webapp2
 import json
 from google.appengine._internal.django.utils import simplejson
@@ -31,12 +16,6 @@ def parse_date(param):
         return datetime.datetime.strptime(param[:19], '%Y-%m-%d %H:%M:%S')
 
     return None
-
-
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        path = os.path.join(os.path.join(os.path.dirname(__file__), 'html'), '../templates/stats_view.html')
-        self.response.out.write(template.render(path, {}))
 
 
 def generate_frequencies_response(keys, weekdays_counter):
@@ -193,8 +172,3 @@ class MigraineData(webapp2.RequestHandler):
 
         self.response.out.write(simplejson.dumps(response))
 
-
-app = webapp2.WSGIApplication([
-                                  ('/', MainHandler),
-                                  ('/migraine-data', MigraineData)
-                              ], debug=True)
