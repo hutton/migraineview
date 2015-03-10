3d691cd9cb18ac0bc1897a7448d874ae1607550c
Simon <simon.hutton@gmail.com>
2015-03-10 22:07:36 +0000

    More on home

--- static/javascript/home.js
+++ static/javascript/home.js
@@ -19,25 +19,25 @@ function hideLogin(){
 function animateTimeline(){
     var rootEl = $('#ani-timeline');
 
-    rootEl.show();
+    rootEl.find('#ani-timeline-text > .ani-header').velocity('transition.slideDownIn', {duration: 1800, delay: 600});
+    rootEl.find('#ani-timeline-text > .ani-body').velocity('transition.slideUpBigIn', {duration: 1800, delay: 1400});
 
-    rootEl.find('.ani-header').velocity('transition.slideDownIn', {duration: 1800, delay: 600});
-    rootEl.find('.ani-body').velocity('transition.slideUpBigIn', {duration: 1800, delay: 1400});
-
-    rootEl.find('.ani-iphone-image-container').velocity('transition.slideUpBigIn', {delay: 3000, duration: 1000});
-    rootEl.find('.ani-iphone-content-image').velocity({'background-position-y': -750}, {duration: 3000, delay: 4000, easing: 'ease-in-out', complete: function(){
-        rootEl.find('.ani-iphone-content-image').velocity({'background-position-y': 0}, {duration: 600, delay: 800, easing: 'ease-in-out'});
-    }});
-    rootEl.find('.ani-buttons').velocity('transition.slideDownIn', {delay: 8000});
-}
-
-function animateReport(){
-    var rootEl = $('#ani-report');
-
-    rootEl.find('.ani-ipad-content-image').velocity({'background-position-y': -720}, {duration: 3000, delay: 0, easing: 'ease-out-expo', complete: function(){
-        rootEl.find('.ani-ipad-content-image').velocity({'background-position-y': 0}, {duration: 600, delay: 800, easing: 'ease-in-out'});
+    rootEl.find('.ani-iphone-image-container').velocity('transition.slideUpBigIn', {delay: 3000, duration: 2000});
+    rootEl.find('.ani-iphone-content-image').velocity({'background-position-y': -250}, {duration: 2000, delay: 4400, easing: 'ease-in-out', complete: function(){
+        rootEl.find('.ani-iphone-content-image').velocity({'background-position-y': 0}, {duration: 800, delay: 1000, easing: 'ease-in-out'});
     }});
 
+    rootEl.find('#ani-timeline-text').velocity('transition.slideRightOut', {duration: 400, delay: 9000, complete: function(){
+        rootEl.find('#ani-report-text').velocity('transition.slideLeftIn', {duration: 400});
+    }});
+
+    rootEl.find('.ani-iphone-report-content-image').velocity('transition.slideLeftIn', {duration: 400, delay: 9000, complete: function(){
+        rootEl.find('.ani-iphone-report-content-image').velocity({'background-position-y': -250}, {duration: 2000, delay: 1400, easing: 'ease-in-out', complete: function(){
+            rootEl.find('.ani-iphone-report-content-image').velocity({'background-position-y': 0}, {duration: 800, delay: 1000, easing: 'ease-in-out'});
+        }});
+    }});
+
+    rootEl.find('.ani-buttons').velocity('transition.slideDownIn', {delay: 12000});
 }
 
 $(document).ready(function(){
@@ -57,6 +57,4 @@ $(document).ready(function(){
     });
 
     animateTimeline();
-
-//    animateReport();
 });
