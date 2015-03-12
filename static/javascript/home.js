4ad52f6d7add919552b23dc6f824172479359e80
Simon <simon.hutton@gmail.com>
2015-03-12 12:36:54 +0000

    Scroll home

--- static/javascript/home.js
+++ static/javascript/home.js
@@ -23,21 +23,43 @@ function animateTimeline(){
     rootEl.find('#ani-timeline-text > .ani-body').velocity('transition.slideUpBigIn', {duration: 1800, delay: 1400});
 
     rootEl.find('.ani-iphone-image-container').velocity('transition.slideUpBigIn', {delay: 3000, duration: 2000});
-    rootEl.find('.ani-iphone-content-image').velocity({'background-position-y': -250}, {duration: 2000, delay: 4400, easing: 'ease-in-out', complete: function(){
-        rootEl.find('.ani-iphone-content-image').velocity({'background-position-y': 0}, {duration: 800, delay: 1000, easing: 'ease-in-out'});
+    rootEl.find('.ani-iphone-content-image').velocity({'background-position-y': -100}, {duration: 500, delay: 4400, easing: 'ease-in-out', complete: function(){
+        rootEl.find('.ani-iphone-content-image').velocity({'background-position-y': 0}, {duration: 200, delay: 100, easing: 'ease-in-out'});
     }});
 
-    rootEl.find('#ani-timeline-text').velocity('transition.slideRightOut', {duration: 400, delay: 9000, complete: function(){
+    rootEl.find('#ani-timeline-text').velocity('transition.slideRightOut', {duration: 400, delay: 6000, complete: function(){
         rootEl.find('#ani-report-text').velocity('transition.slideLeftIn', {duration: 400});
     }});
 
-    rootEl.find('.ani-iphone-report-content-image').velocity('transition.slideLeftIn', {duration: 400, delay: 9000, complete: function(){
-        rootEl.find('.ani-iphone-report-content-image').velocity({'background-position-y': -250}, {duration: 2000, delay: 1400, easing: 'ease-in-out', complete: function(){
-            rootEl.find('.ani-iphone-report-content-image').velocity({'background-position-y': 0}, {duration: 800, delay: 1000, easing: 'ease-in-out'});
+    rootEl.find('.ani-iphone-report-content-image').velocity('transition.slideLeftIn', {duration: 400, delay: 6000, complete: function(){
+        rootEl.find('.ani-iphone-report-content-image').velocity({'background-position-y': -100}, {duration: 500, delay: 1400, easing: 'ease-in-out', complete: function(){
+            rootEl.find('.ani-iphone-report-content-image').velocity({'background-position-y': 0}, {duration: 200, delay: 100, easing: 'ease-in-out'});
         }});
     }});
 
-    rootEl.find('.ani-buttons').velocity('transition.slideDownIn', {delay: 12000});
+    rootEl.find('.ani-buttons').velocity('transition.slideDownIn', {delay: 9000});
+}
+
+function bindScroll(){
+    var timelineHeight = 780;
+    var reportHeight = 1100;
+
+    var timelineContent = $('.ani-iphone-content-image');
+    var reportContent = $('.ani-iphone-report-content-image');
+
+    $(window).scroll(function() {
+        var scrollPos = $(window).scrollTop();
+        var imageTopOffset = $('.ani-iphone-image').offset().top + 200;
+
+        var percentScroll = scrollPos / imageTopOffset;
+
+        var timelineScrollPos =  Math.min(timelineHeight * percentScroll, timelineHeight);
+        var reportScrollPos =  Math.min(reportHeight * percentScroll, reportHeight);
+
+        timelineContent.css("background-position-y", -timelineScrollPos);
+        reportContent.css("background-position-y", -reportScrollPos);
+    });
+
 }
 
 $(document).ready(function(){
@@ -48,6 +70,8 @@ $(document).ready(function(){
         setDimensions();
     });
 
+    bindScroll();
+
     $('#login-link').click(function(){
       showLogin();
     });
@@ -57,4 +81,6 @@ $(document).ready(function(){
     });
 
     animateTimeline();
+
+
 });
